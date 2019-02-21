14c7f8f685f0c033ededb12c9d5e701f2c687c7e
Qingyu Deng <bitdqy@hotmail.com>
2019-02-21 16:02:33 +0800

    test: improve test of MessageRelayService

--- tests/services/message-relay.spec.ts
+++ tests/services/message-relay.spec.ts
@@ -6,11 +6,19 @@ describe('MessageRelayService', () => {
   it('should relay the message after manipulated by processor', () => {
     const telegramBot = new TelegramBot('12345')
     const messageRelaySrv = createMessageRelayService({
-      processor: ({ message }) => 'called',
+      processor: ({ message }) => `received: ${message}`,
       bot: telegramBot,
       chatId: '123456'
     })
     messageRelaySrv({ message: 'hello world' })
-    expect(telegramBot.sendMessage).toBeCalledWith('123456', 'called')
+    expect(telegramBot.sendMessage).toBeCalledWith(
+      '123456',
+      'received: hello world'
+    )
+    messageRelaySrv({ message: 'one more time' })
+    expect(telegramBot.sendMessage).toBeCalledWith(
+      '123456',
+      'received: one more time'
+    )
   })
 })
